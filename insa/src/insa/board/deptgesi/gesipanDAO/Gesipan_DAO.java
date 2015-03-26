f7aa585f1ace65f468e06ec5267adc6bf4c58eb1
Kimyoungwoo <kyw6390@naver.com>
2015-03-26 14:12:35 +0900

    dqwdqwd

--- insa/src/insa/board/deptgesi/gesipanDAO/Gesipan_DAO.java
+++ insa/src/insa/board/deptgesi/gesipanDAO/Gesipan_DAO.java
@@ -1,12 +1,13 @@
 package insa.board.deptgesi.gesipanDAO;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public class Gesipan_DAO {
+	private SqlSession sqlSession;
 		
 }
